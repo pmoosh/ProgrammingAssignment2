6b26988d51cea4d31d9ae034e23ac03973d4d105
Peter Mooshammer <pmoosh@yahoo.com>
2014-04-22 14:39:04 -0700

    Modified .R file for assignment. Needs more testing

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,23 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        # initialise the inversion flag
+        inv <- NULL
+        set <- function(y) {
+            # set the martix x
+            x <<- y
+            # and clear the inversion flag
+            inv <<- NULL
+        
+        }
+        # returns the matrix x
+        get <- function() x
+        # stores the inverted matrix
+        setinv <- function(ix) inv <<- ix
+        # returns the cached inverted matrix
+        getinv <- function() inv
+    
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
@@ -12,4 +28,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+            message("We inverted the matrix already")
+            return(inv)
+        }
+        mat <- x$get()
+        inv <- solve(mat)
+        x$setinv(inv)
+        inv
+            
+        
+    
 }
