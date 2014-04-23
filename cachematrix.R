652949d39cea17b626c76298ee3c3b2e622ea9f7
Peter Mooshammer <pmoosh@yahoo.com>
2014-04-22 22:58:28 -0700

    Added comments and increased test coverage

--- cachematrix.R
+++ cachematrix.R
@@ -1,42 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This set of R utilities optimizes the inversion of square matrixes by 
+## caching already converted matrixes
 
-## Write a short comment describing this function
+## Calling this function with a matrix as parameter will store the matrix. 
+## The set function will achieve the same. 
+## This function stores the original matrix as well as the inverted
 
 makeCacheMatrix <- function(x = matrix()) {
         # initialise the inversion flag
         inv <- NULL
+        
+        # set the matrix x
         set <- function(y) {
-            # set the martix x
+        
             x <<- y
             # and clear the inversion flag
             inv <<- NULL
         
         }
+
         # returns the matrix x
         get <- function() x
+
         # stores the inverted matrix
         setinv <- function(ix) inv <<- ix
+
         # returns the cached inverted matrix
         getinv <- function() inv
-    
+        
         list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## A cached version of solve(). Uses the a matrix stored in makeCacheMatrix 
+## inverts it and saves it in makeCacheMatrix. Before it runs the inversion
+## process again, it checks with makeCacheMatrix if it already 
+## contains a cached copy
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        
         inv <- x$getinv()
+        
+        # ckeck it is already in the cache
         if(!is.null(inv)) {
             message("We inverted the matrix already")
             return(inv)
         }
+        
+        # invert the matrix
         mat <- x$get()
+        
+        # if there is an error we let the caller of the function deal with it
         inv <- solve(mat)
+        
+        # store the inverted matrix in the cache
         x$setinv(inv)
+        
+        # return the inverted matrix
         inv
             
         
